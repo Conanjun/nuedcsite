0f158d19f1386cec42a35b644fb80fe5f718f8ec
hackeris <hackeris@qq.com>
2014-12-10 23:35:30 +0800

    first commit

---
+++ NuedcSite/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'NuedcSite.views.home', name='home'),
+    # url(r'^NuedcSite/', include('NuedcSite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)

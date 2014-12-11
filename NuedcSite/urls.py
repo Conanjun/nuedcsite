bb3c3b8153545c680fff8146a50fdeae5768ad93
hackeris <hackeris@qq.com>
2014-12-11 21:32:08 +0800

    add home and slide show

--- NuedcSite/urls.py
+++ NuedcSite/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'NuedcSite.views.home', name='home'),
+    url(r'^$', 'NuedcSite.views.home', name='home'),
     # url(r'^NuedcSite/', include('NuedcSite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

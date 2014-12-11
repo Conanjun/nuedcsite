8af12e05b0cb4b0b59d3001482400ed8f35382f9
hackeris <hackeris@qq.com>
2014-12-11 12:13:46 +0800

    add tabs

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    
     url(r'^news/', 'posts.views.news_index', name='news_index'),
     url(r'^news_item/','posts.views.show_article',name='show_article'),
     url(r'notify/','posts.views.notifies_index',name='notifies_index'),

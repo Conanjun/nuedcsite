ca0afa7a9131b37a9a8fa32486f5f000959ec059
hackeris <hackeris@qq.com>
2014-12-11 12:09:17 +0800

    add notifies views

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     
     url(r'^news/', 'posts.views.news_index', name='news_index'),
-    url(r'^news_article/','posts.views.show_article',name='show_article'),
+    url(r'^news_item/','posts.views.show_article',name='show_article'),
+    url(r'notify/','posts.views.notifies_index',name='notifies_index'),
+    url(r'notify_item/','posts.views.show_notify',name='show_notify'),
 )
--- posts/views.py
+++ posts/views.py
@@ -11,7 +11,7 @@ from posts.models import *
 
 COUNT_PER_PAGE = 5
 after_range_num = 5
-befor_range_num = 4
+before_range_num = 4
 
 # Create your views here.
 def news_index(request):
@@ -27,10 +27,31 @@ def news_index(request):
     except (EmptyPage, InvalidPage):
         this_page = paginator.page(paginator.num_pages)
     if page >= after_range_num:
-        page_range = paginator.page_range[page-after_range_num:page+befor_range_num]
+        page_range = paginator.page_range[page-after_range_num:page+before_range_num]
     else:
-        page_range = paginator.page_range[0:int(page)+befor_range_num]
+        page_range = paginator.page_range[0:int(page)+before_range_num]
     return render_to_response('news_list.html',{"nav":"news","news_list":news_list,"length":page_range})
 
 def show_article(request):
+    return render_to_response('news_content.html',{"nav":"news"})
+
+def notifies_index(request):
+    news_list = NewsPost.objects.all()
+    paginator = Paginator(news_list, COUNT_PER_PAGE)
+    pages_count = paginator.num_pages
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        this_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        this_page = paginator.page(paginator.num_pages)
+    if page >= after_range_num:
+        page_range = paginator.page_range[page-after_range_num:page+before_range_num]
+    else:
+        page_range = paginator.page_range[0:int(page)+before_range_num]
+    return render_to_response('news_list.html',{"nav":"news","news_list":news_list,"length":page_range})
+
+def show_notify(request):
     return render_to_response('news_content.html',{"nav":"news"})

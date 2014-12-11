001dae1ea5f0e52797189e39215330e77ca87ede
hackeris <hackeris@qq.com>
2014-12-11 12:12:26 +0800

    add blank lines to code

--- posts/models.py
+++ posts/models.py
@@ -2,30 +2,34 @@ from django.db import models
 from tinymce.models import HTMLField
 from django.contrib import admin
 
+
 # Create your models here.
 class NewsPost(models.Model):
-	title = models.CharField(max_length=50)
-	content = HTMLField()
-	uptime = models.DateTimeField(auto_now=True)
-	rating = models.IntegerField(default=0)
-	
-	def __unicode__(self):
-		return self.title
+    title = models.CharField(max_length=50)
+    content = HTMLField()
+    uptime = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
 
 class NotifyPost(models.Model):
-	title = models.CharField(max_length=50)
-	content = HTMLField()
-	uptime = models.DateTimeField(auto_now=True)
-	rating = models.IntegerField(default=0)
-	
-	def __unicode__(self):
-		return self.title	
+    title = models.CharField(max_length=50)
+    content = HTMLField()
+    uptime = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
 
 class NewsPostAdmin(admin.ModelAdmin):
-	list_display = ["title","uptime"]
+    list_display = ["title","uptime"]
+
 
 class NotifyPostAdmin(admin.ModelAdmin):
-	list_display = ["title","uptime"]
+    list_display = ["title","uptime"]
 
 admin.site.register(NewsPost,NewsPostAdmin)
 admin.site.register(NotifyPost,NotifyPostAdmin)

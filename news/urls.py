8c12fc8927749586413060e3419f4aba93a5da45
hackeris <hackeris@qq.com>
2014-12-11 09:37:30 +0800

    add files.views.index

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    # Examples:
+    
     url(r'^$', 'news.views.index', name='news_index'),
     url(r'^article/','news.views.show_article',name='show_article'),
 )

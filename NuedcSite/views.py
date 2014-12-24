f910707702462c26388a95c349352e7bcc7d770d
hackeris <hackeris@qq.com>
2014-12-24 13:46:36 +0800

    5 posts in home page

--- NuedcSite/views.py
+++ NuedcSite/views.py
@@ -9,8 +9,8 @@ import os
 
 def home(request):
     news_list = NewsPost.objects.all()
-    max_rating_news = NewsPost.objects.order_by("-rating")[0:1]
+    max_rating_news = NewsPost.objects.order_by("-rating")[0:5]
     notify_list=NotifyPost.objects.all()
-    max_rating_notify = NotifyPost.objects.order_by("-rating")[0:1]
+    max_rating_notify = NotifyPost.objects.order_by("-rating")[0:5]
 
     return render_to_response('home.html',{"nav":"home","news_list":news_list,"max_rating_news":max_rating_news,"notify_list":notify_list,"max_rating_notify":max_rating_notify})
